5f77496709524d3010478cca15d5bb0032bf7b27
zunair shahid <zunair.shahid.c@miracommerce.com>
2023-05-23 01:13:41 +0500

    new changes

--- assets/filters-script.js
+++ assets/filters-script.js
@@ -1,5 +1,27 @@
+function showAlert(name) {
+	var alertBox = $('#alertBox');
+    $(alertBox).show();
+    $(alertBox).html('<p>' + name + ' has been added into cart. </p>');
+    setTimeout(() => {
+      $(alertBox).hide();
+    }, 7000);
+  }
+  
+  function closeAlert() {
+	var alertBox = $('#alertBox');
+	  $(alertBox).hide();
+  }
+
+  function updateCartCounter() {
+    jQuery.getJSON("/cart.js", function (cart) {
+      let items_count = cart.item_count;
+      $(".cart-counter-number span").text(items_count);
+    })
+  }
+  
 $(document).on('click', '.product-item .add-to-cart-btn', function () {
   var product_id = $(this).data('id');
+  var product_name = $(this).data('name');
   var data = {
     "id": product_id,
     "quantity": 1
@@ -10,7 +32,9 @@ $(document).on('click', '.product-item .add-to-cart-btn', function () {
     data: data,
     dataType: 'json',
     success: function () {
-      alert('Product added to cart successfully!');
+      // alert('Product added to cart successfully!');
+      showAlert(product_name);
+      updateCartCounter();
     },
     error: function (XMLHttpRequest, textStatus) {
       alert('Error adding product to cart: ' + textStatus);
